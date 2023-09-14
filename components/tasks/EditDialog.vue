902517b0514f0be6d1e8b189ead611d227749e8e
JhnnyPzG <jpaz152@soyudemedellin.edu.co>
2023-09-14 18:32:43 -0500

    Update

--- components/tasks/EditDialog.vue
+++ components/tasks/EditDialog.vue
@@ -3,15 +3,15 @@
         <v-dialog v-model="openDialog" width="80%">
             <v-card>
                 <v-card-item>
-                    <v-card-title>Edicion de tareas {{ props.task.name }}</v-card-title>
-                    <v-card-subtitle>Formulario para la gestión de tareas de los estudiantes.</v-card-subtitle>
+                    <v-card-title>Edición de tarea {{ editingTask.name }}</v-card-title>
                 </v-card-item>
                 <v-card-text>
                     <form action="javascript:void(0)" @submit="saveTask">
 
                         <v-text-field label="Nombre" v-model="editingTask.name" required placeholder="Ingrese su nombre" />
 
-                        <v-text-field label="Correo" v-model="editingTask.email" type="email" placeholder="Ingrese su correo" />
+                        <v-text-field label="Correo" v-model="editingTask.email" type="email"
+                            placeholder="Ingrese su correo" />
 
                         <v-textarea label="Descripción" v-model="editingTask.description" cols="10" rows="3"></v-textarea>
 
@@ -36,42 +36,44 @@
                     </form>
                 </v-card-text>
                 <v-card-actions>
-                    <v-btn color="primary" block @click="dialog = false">Cerrar</v-btn>
+                    <v-btn color="primary" block @click="openDialog = false">Cerrar</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
     </div>
 </template>
 <script setup>
+
 const props = defineProps({
-    dialog:{
+    dialog: {
         type: Boolean,
         required: true
     },
-    task:{
+    task: {
         type: Object,
         required: true
     }
 })
 
-onBeforeMount (() => {
+const emit = defineEmits(['update'])
+
+onBeforeMount(() => {
     editingTask.value = props.task
     openDialog.value = props.dialog
 })
 
+
 import axios from "axios";
 // Variables
-const editingTask = ref({  })
-//const dialog = ref(false)
-const openDialog = computed(() => {
-    return props.dialog
-})
+const editingTask = ref({})
+const openDialog = ref(false)
 
 const saveTask = async () => {
-  const url = `http://localhost:3001/tasks/${editingTask.value.id}`
-  const result = await axios.put(url, editingTask.value)
-  console.log(result);
-  openDialog.value = false
+    const url = `http://localhost:3001/tasks/${editingTask.value.id}`
+    const result = await axios.put(url, editingTask.value)
+    console.log(result);
+    openDialog.value = false
+    emit('update', true)
 }
 
 
--- components/tasks/Table.vue
+++ components/tasks/Table.vue
@@ -1,37 +1,37 @@
 <template>
-  <div class="mt-3">
+    <div class="mt-3">
 
-      <h3>Listado de tareas</h3>
+        <h3>Listado de tareas</h3>
 
-      <v-table>
-          <thead>
-              <tr>
-                  <th class="text-left">
-                      Nombre
-                  </th>
-                  <th class="text-left">
-                      Descripción
-                  </th>
-                  <th class="text-left">
-                      Acciones
-                  </th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr v-for="item in tasks" :key="item.id">
-                  <td>{{ item.name }}</td>
-                  <td>{{ item.description }}</td>
-                  <td>
-                      <v-btn icon="mdi-pencil" variant="text" @click="editTask(item)">
-                      </v-btn>
-                      <v-btn icon="mdi-delete-off" variant="text" @click="deleteTask(item)">
-                      </v-btn>
-                  </td>
-              </tr>
-          </tbody>
-      </v-table>
-      <tasks-edit-dialog v-if="editingTask!= null" :dialog="isEdit" :task="editingTask"/>
-  </div>
+        <v-table>
+            <thead>
+                <tr>
+                    <th class="text-left">
+                        Nombre
+                    </th>
+                    <th class="text-left">
+                        Descripción
+                    </th>
+                    <th class="text-left">
+                        Acciones
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in tasks" :key="item.id">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.description }}</td>
+                    <td>
+                        <v-btn icon="mdi-pencil" variant="text" @click="editTask(item)">
+                        </v-btn>
+                        <v-btn icon="mdi-delete-off" variant="text" @click="deleteTask(item)">
+                        </v-btn>
+                    </td>
+                </tr>
+            </tbody>
+        </v-table>
+        <tasks-edit-dialog v-if="editingTask != null" :dialog="isEdit" :task="editingTask" @update="updateTask" />
+    </div>
 </template>
 <script setup>
 import axios from "axios";
@@ -41,24 +41,34 @@ const isEdit = ref(false)
 const editingTask = ref(null)
 
 onBeforeMount(() => {
-  loadTasks()
+    loadTasks()
 })
 
 const loadTasks = async () => {
-  const url = "http://localhost:3001/tasks"
-  const { data } = await axios.get(url)
-  tasks.value = data
+    const url = "http://localhost:3001/tasks"
+    const { data } = await axios.get(url)
+    tasks.value = data
 }
-const deleteTask =async (item) => {
-  const url = `http://localhost:3001/tasks/${item.id}`
-  const { data } = await axios.delete(url)
- loadTasks()
+const deleteTask = async (item) => {
+    const url = `http://localhost:3001/tasks/${item.id}`
+    const { data } = await axios.delete(url)
+    loadTasks()
+}
+const editTask = async (item) => {
+    isEdit.value = true
+    editingTask.value = { ...item }
+    console.log(editingTask.value);
+
 }
 
-const editTask =async (item) => {
-  isEdit.value=true
-  editingTask.value={ ...item}
-  console.log(editingTask.value);
-
+const updateTask = (isUpdated) => {
+    console.log(isUpdated);
+    isEdit.value=false
+    editingTask.value=null
+    loadTasks()
 }
+</script>           
+ 
+
+
 </script>
